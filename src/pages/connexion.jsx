02295e3e339a17779c43628f38701de641b9098e
belghorafbryan1996 <belghorafbryan1996@gmail.com>
2026-06-16 14:21:16 +0200

    fix: passage en lecture storage local sur la page connexion aussi

--- src/pages/connexion.jsx
+++ src/pages/connexion.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 
 export default function Connexion() {
   const [email, setEmail] = useState("")
   const [motDePasse, setMotDePasse] = useState("")
   const [erreur, setErreur] = useState("")
-  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log("Envoi :", { email, mot_de_passe: motDePasse })
     
     const response = await fetch("https://hellobad.alwaysdata.net/connexion_clients.php", {
       method: "POST",
@@ -24,26 +21,26 @@ export default function Connexion() {
     
     if (data.error) {
       setErreur(data.error)
-   } else {
-  localStorage.setItem('user', JSON.stringify(data.client))
-  if (data.client.role === 'admin') {
-    navigate("/admin")
-  } else {
-    navigate("/")
+    } else {
+      localStorage.setItem('user', JSON.stringify(data.client))
+      if (data.client.role === 'admin') {
+        window.location.href = '/admin'
+      } else {
+        window.location.href = '/'
+      }
+    }
   }
-}
-  }
+
   return (
     <section className="bg-gray-50">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               Connexion
             </h1>
 
-            {/* {erreur && <p className="text-red-500 text-sm">{erreur}</p>} */}
+            {erreur && <p className="text-red-500 text-sm">{erreur}</p>}
 
             <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
               <div>
@@ -52,7 +49,6 @@ export default function Connexion() {
                 </label>
                 <input
                   type="email"
-                  name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
                   placeholder="email@exemple.com"
@@ -67,7 +63,6 @@ export default function Connexion() {
                 </label>
                 <input
                   type="password"
-                  name="password"
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
